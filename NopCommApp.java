0403aef44755172e07f250447246e7c2335f4ae1
lalitham22 <lallu2209@gmail.com>
2023-11-20 05:02:00 -0800

    Seleniumtest1

---
+++ NopCommApp.java
@@ -0,0 +1,22 @@
+package crossbrowserTest;
+
+import org.testng.annotations.Test;
+
+
+
+public class NopCommApp extends BaseTestCrossbrowser {
+	
+	@Test
+	public void verifyloginfeature() {
+		System.out.println("login test case execution");
+		
+	
+	
+		
+
+	}
+	
+	
+	
+
+}
